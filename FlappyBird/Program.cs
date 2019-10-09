6725d448f58a0822cb5f1e8b343060a76498e23d
Mike Gorzelsky <m.d.gorzelsky@gmail.com>
2019-10-09 08:16:58 -0700

    Add project files.

---
+++ FlappyBird/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlappyBird
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
