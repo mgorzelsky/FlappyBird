89fbe916a7d75651d086a168f72d50895b909722
Matthew Juel <matthewjuel@g5pb13.mlearning.nh>
2019-10-09 15:52:15 -0700

    Split Bird and Wall Class Files

--- FlappyBird/Program.cs
+++ FlappyBird/Program.cs
@@ -8,9 +8,14 @@ namespace FlappyBird
         static void Main(string[] args)
         {
             ConsoleKey action = ConsoleKey.N;
-            
+
+            Timer gameTimer = new Timer(400);
+            gameTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            gameTimer.Enabled = true;
+
             Console.CursorVisible = false;
-            Bird flappy = new Bird(5, 5, 500);
+            Bird flappy = new Bird(5, 5, 200);
+            Wall wall1 = new Wall(90, 0, 40, 100);
 
             while (action != ConsoleKey.Q)
             {
@@ -24,86 +29,22 @@ namespace FlappyBird
 
         }
 
-    }
-
-    class Bird
-    {
-        private int positionX;
-        private int positionY;
-        private int priorY;
-        private char icon = '>';
-        private System.Timers.Timer timer;
-
-        public int PositionY {
-
-            get
-            {
-                return positionY;
-            }
-
-            set
-            {
-                positionY = value;
-            }
-
-        }
-
-        public Bird(int x, int y, int ms)
+        public static void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
-            positionX = x;
-            positionY = y;
-            timer = new System.Timers.Timer(ms);
-            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            timer.Enabled = true;
+            Console.Clear();
         }
-
-        public void flap()
-        {
-            priorY = PositionY;
-
-            PositionY -= 3;
-
-        }
-
-        public void fall()
-        {
-            priorY = positionY;
-            PositionY += 1;
-            Console.SetCursorPosition(positionX, positionY);
-            Console.Write(icon);
-            Console.SetCursorPosition(positionX, priorY);
-            Console.Write(" ");
-
-        }
-
-        public void draw()
-        {
-            Console.SetCursorPosition(positionX, positionY);
-            Console.Write(icon);
-
-            Console.SetCursorPosition(positionX, priorY);
-            Console.Write(" ");
-        }
-
-        public void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            fall();
-        } 
 
     }
 
-    //class Wall
+    //class Bird
     //{
     //    private int positionX;
     //    private int positionY;
-    //    private int priorX;
-    //    private int height;
-    //    private int holeHeight;
-    //    private char icon = '|';
+    //    private int priorY;
+    //    private char icon = '>';
     //    private System.Timers.Timer timer;
 
-    //    public int PositionY
-    //    {
+    //    public int PositionY {
 
     //        get
     //        {
@@ -117,7 +58,7 @@ namespace FlappyBird
 
     //    }
 
-    //    public Wall(int x, int y, int ms)
+    //    public Bird(int x, int y, int ms)
     //    {
     //        positionX = x;
     //        positionY = y;
@@ -138,35 +79,91 @@ namespace FlappyBird
     //    {
     //        priorY = positionY;
     //        PositionY += 1;
-    //        Console.SetCursorPosition(positionX, positionY);
-    //        Console.Write(icon);
-    //        Console.SetCursorPosition(positionX, priorY);
-    //        Console.Write(" ");
 
+    //        draw();
     //    }
 
     //    public void draw()
     //    {
+    //        Console.SetCursorPosition(positionX, positionY);
+    //        Console.Write(icon);
 
-    //        for(int i = 0; i < height; i++)
-    //        {
-    //            Console.SetCursorPosition(positionX, positionY);
-    //            Console.Write(icon);
-    //        }
-
-
-    //        Console.SetCursorPosition(positionX, priorX);
+    //        Console.SetCursorPosition(positionX, priorY);
     //        Console.Write(" ");
     //    }
 
     //    public void OnTimedEvent(Object source, ElapsedEventArgs e)
     //    {
     //        fall();
+    //    } 
+
+    //}
+
+    //class Wall
+    //{
+    //    private int positionX;
+    //    private int positionY;
+    //    private int priorX;
+    //    private int height;
+    //    private int holeHeight = 7;
+    //    private char icon = '|';
+    //    private System.Timers.Timer timer;
+
+    //    public int PositionX
+    //    {
+
+    //        get
+    //        {
+    //            return positionX;
+    //        }
+
+    //        set
+    //        {
+    //            positionX = value;
+    //        }
+
     //    }
 
-    //    public string toString()
+    //    public Wall(int x, int y, int height, int ms)
     //    {
-    //        Console.SetCursorPosition()
+    //        positionX = x;
+    //        positionY = y;
+    //        this.height = height;
+    //        timer = new System.Timers.Timer(ms);
+    //        timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+    //        timer.Enabled = true;
+    //    }
+
+
+    //    public void draw()
+    //    {
+
+    //        for (int i = 0; i < height; i++)
+    //        {
+    //            Console.SetCursorPosition(priorX, positionY + i);
+    //            Console.Write(" ");
+    //        }
+
+    //        for (int i = 0; i < height; i++)
+    //        {
+    //            Console.SetCursorPosition(positionX, positionY + i);
+    //            Console.Write(icon);
+
+    //            if(i == holeHeight)
+    //            {
+    //                Console.Write(" ");
+    //            }
+    //        }
+
+    //    }
+
+    //    public void OnTimedEvent(Object source, ElapsedEventArgs e)
+    //    {
+    //        priorX = PositionX;
+
+    //        PositionX -= 1;
+
+    //        draw();
     //    }
 
     //}
