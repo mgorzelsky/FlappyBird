60c5aaec5802fc98275771c1d9500ae29715af1e
robschroeder333 <theguyonthecouch7@gmail.com>
2019-10-09 13:19:31 -0700

    feat(program): add basic start logic

--- FlappyBird/Program.cs
+++ FlappyBird/Program.cs
@@ -13,6 +13,23 @@ namespace FlappyBird
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(width, height);
             Console.SetWindowSize(width, height);
+
+            Game game = new Game();
+            Console.WriteLine("Welcome to flappy bird in the terminal.");
+            do
+            {
+                game.Play();
+            } while (playAgain);
+
+            if (game.HighScore < 5)
+                Console.Write($"Booooooooo! Your high score was {game.HighScore}");
+            else if (game.HighScore >= 5 && game.HighScore < 15)
+                Console.Write($"Nice try! Your high score was {game.HighScore}");
+            else if (game.HighScore >= 15 && game.HighScore < 30)
+                Console.Write($"Wow! Thats pretty good! Your high score was {game.HighScore}");
+            else
+                Console.Write($"You are really good at this! Your high score was {game.HighScore}");
+
         }
     }
 }
