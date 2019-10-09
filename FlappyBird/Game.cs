554601cb6579b352233845bfac910211bc5097f4
Mike Gorzelsky <m.d.gorzelsky@gmail.com>
2019-10-09 13:08:16 -0700

    Created the file for the Game class with an initial method for starting the game.

---
+++ FlappyBird/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Game
+    {
+        public void PlayGame()
+        {
+
+        }
+    }
+}
